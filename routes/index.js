35a874578d5a1ecd12f0c938d1a67a68b4915fef
vparfenovAtGoogle <53707044+vparfenovAtGoogle@users.noreply.github.com>
2019-09-07 13:16:06 -0400

    Update index.js
    Done -> Built

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Done with Node.JS and Express' });
+  res.render('index', { title: 'Built with Node.JS and Express' });
 });
 
 module.exports = router;
